aa8ff2a3240f3fae65db55f46606cb9c4e242209
JackieGCortez <gc1496042021@unab.edu.sv>
2021-08-28 10:35:24 -0600

    compra
    descuento

---
+++ ejercio de descuento/Estructura.py
@@ -0,0 +1,10 @@
+numero1 = int(input(">=100"))
+numero2 = int(input("<100"))
+
+if numero1 >= numero2 :
+    print ("El dato uno es mas mayor que el dato dos, se aplicara descuento 10%, numero1")
+    else:
+        print ("El dato dos en menor que el dato uno no aplicara, numero2")
+
+pagar = numero1(descuento)
+print(pagar)
